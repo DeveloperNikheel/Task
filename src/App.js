b4d80e415ba204b3081a767d08b266fbcff645cd
DeveloperNikheel <105104293+DeveloperNikheel@users.noreply.github.com>
2022-09-06 11:32:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './App.css';
+import Title from './Components/Title';
+import Form from './Components/Signup';
+import Login from './Components/Login';
+import Menu from './Components/Menu';
+
+function App () {
+
+    return (
+      <div>
+        
+        <Form />
+        <Login />
+        <Menu />
+      </div>
+      
+    );
+  }
+
+
+
+export default App;
+
+
---
+++ src/Components/Title.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Title.css'
+
+function Title() {
+  return (
+
+    <div>
+      <div className="Header">
+        <header>
+        <img src="https://img.icons8.com/external-flat-andi-nur-abdillah/64/000000/external-cooking-kitchen-flat-flat-andi-nur-abdillah.png" alt='logo'/>
+          <h5 className="brand-title">Food's Restaurant</h5>
+          
+        </header>
+      </div>
+    </div>
+  )
+}
+
+export default Title
+
